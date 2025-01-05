1088ba01f0862a600f72a300fbd3b8f9eae6c9a5
michael-zenge <michael.zenge@gmail.com>
2025-01-05 16:23:47 +0100

    Change private to protected

--- snake.py
+++ snake.py
@@ -3,22 +3,22 @@ import pygame
 
 class Snake:
     def __init__(self, sprite_size, head_color, body_color, apple_color, wall_color, speed=10, speed_inc=0.5):
-        self.__sprite_size = sprite_size
-        self.__apple_color = pygame.color.Color(apple_color)
-        self.__wall_color = wall_color
-        self.__speed = speed # framerate in fps
-        self.__speed_inc = speed_inc # speed increment
+        self._sprite_size = sprite_size
+        self._apple_color = pygame.color.Color(apple_color)
+        self._wall_color = wall_color
+        self._speed = speed # framerate in fps
+        self._speed_inc = speed_inc # speed increment
         
-        self.__screen = pygame.display.get_surface()
-        self.__screen_size = self.__screen.get_size()
+        self._screen = pygame.display.get_surface()
+        self._screen_size = self._screen.get_size()
                        
-        self.__head_color = pygame.color.Color(head_color)
-        self.__head = pygame.Surface(self.__sprite_size)
-        self.__head.fill(self.__head_color)
+        self._head_color = pygame.color.Color(head_color)
+        self._head = pygame.Surface(self._sprite_size)
+        self._head.fill(self._head_color)
         
-        self.__body_color = pygame.color.Color(body_color)
-        self.__body = pygame.Surface(self.__sprite_size)
-        self.__body.fill(self.__body_color)        
+        self._body_color = pygame.color.Color(body_color)
+        self._body = pygame.Surface(self._sprite_size)
+        self._body.fill(self._body_color)        
 
         # Create sprite object
         self.snake = []
@@ -26,45 +26,45 @@ class Snake:
 
     @property
     def sprite_size(self):
-        return self.__sprite_size
+        return self._sprite_size
 
     @property
     def body_color(self):
-        return self.__body_color
+        return self._body_color
     
     @property
     def wall_color(self):
-        return self.__wall_color
+        return self._wall_color
     
     @property
     def direction(self):
         """Get the current direction."""
-        return self.__direction
+        return self._direction
         
     @direction.setter
     def direction(self, new_direction, advanced=False):
         """Set a new direction."""
         if advanced:
             if new_direction == pygame.K_UP and self.direction != pygame.K_DOWN:
-                self.__direction = new_direction
+                self._direction = new_direction
                 print("UP")
             elif new_direction == pygame.K_LEFT and self.direction != pygame.K_RIGHT:
-                self.__direction = new_direction
+                self._direction = new_direction
                 print("LEFT")        
             elif new_direction == pygame.K_RIGHT and self.direction != pygame.K_LEFT:
-                self.__direction = new_direction
+                self._direction = new_direction
                 print("RIGHT")
             elif new_direction == pygame.K_DOWN and self.direction != pygame.K_UP:
-                self.__direction = new_direction        
+                self._direction = new_direction        
                 print("DOWN")                
         else:
-            self.__direction = new_direction
+            self._direction = new_direction
 
     def reset(self):
         self.snake.clear()
         for ii in range (0,5):
-            self.snake.append((int(self.__screen_size[0]/4 + ii*self.__sprite_size[0]), int(self.__screen_size[1]/2)))
-        self.__direction = pygame.K_RIGHT
+            self.snake.append((int(self._screen_size[0]/4 + ii*self._sprite_size[0]), int(self._screen_size[1]/2)))
+        self._direction = pygame.K_RIGHT
 
     def do(self, event_key):
         if event_key in {pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT}:
@@ -74,35 +74,35 @@ class Snake:
         # Move snake
         self.move()
 
-        if self.__screen.get_at(self.snake[-1]) in [self.__body_color, self.__wall_color]:
+        if self._screen.get_at(self.snake[-1]) in [self._body_color, self._wall_color]:
             return False
                                
         # Eat apple        
-        if self.__screen.get_at(self.snake[-1]) == self.__apple_color:
+        if self._screen.get_at(self.snake[-1]) == self._apple_color:
             self.snake.insert(0, (self.snake[0])) # grow snake
-            self.__speed += self.__speed_inc
+            self._speed += self._speed_inc
 
         # Update speed
-        pygame.time.Clock().tick(self.__speed) # framerate in fps
+        pygame.time.Clock().tick(self._speed) # framerate in fps
 
         return True
 
     def draw(self):
         for pos in self.snake[0:-1]:
-            self.__screen.blit(self.__body, pos)
+            self._screen.blit(self._body, pos)
         # Do not draw head over apple or wall
-        if self.__screen.get_at(self.snake[-1]) not in [self.__body_color, self.__wall_color, self.__apple_color]:
-            self.__screen.blit(self.__head, self.snake[-1])
+        if self._screen.get_at(self.snake[-1]) not in [self._body_color, self._wall_color, self._apple_color]:
+            self._screen.blit(self._head, self.snake[-1])
 
     def move(self):
         if self.direction == pygame.K_RIGHT:
-            self.snake.append((self.snake[-1][0] + self.__sprite_size[0] , self.snake[-1][1]))
+            self.snake.append((self.snake[-1][0] + self._sprite_size[0] , self.snake[-1][1]))
         elif self.direction == pygame.K_UP:
-            self.snake.append((self.snake[-1][0] , self.snake[-1][1] - self.__sprite_size[1]))
+            self.snake.append((self.snake[-1][0] , self.snake[-1][1] - self._sprite_size[1]))
         elif self.direction == pygame.K_DOWN:
-            self.snake.append((self.snake[-1][0] , self.snake[-1][1] + self.__sprite_size[1]))
+            self.snake.append((self.snake[-1][0] , self.snake[-1][1] + self._sprite_size[1]))
         elif self.direction == pygame.K_LEFT:        
-            self.snake.append((self.snake[-1][0] - self.__sprite_size[0] , self.snake[-1][1]))
+            self.snake.append((self.snake[-1][0] - self._sprite_size[0] , self.snake[-1][1]))
         self.snake.pop(0)
    
 # Copyright (c) 2024 michael-zenge, permission granted under MIT license
