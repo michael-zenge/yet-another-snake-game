07bcfa643ffdcbd8e24839c45a7e19e5becc89c5
michael-zenge <michael.zenge@gmail.com>
2025-01-06 20:37:40 +0100

    Unit tests for Snake object

--- snake.py
+++ snake.py
@@ -2,12 +2,14 @@
 import pygame
 
 class Snake:
-    def __init__(self, sprite_size, head_color, body_color, apple_color, wall_color, speed=10, speed_inc=0.5):
+    def __init__(self, sprite_size, head_color, body_color, apple_color, wall_color, speed=10, speed_inc=0.5, save_update=False):
         self._sprite_size = sprite_size
         self._apple_color = pygame.color.Color(apple_color)
-        self._wall_color = wall_color
+        self._wall_color = pygame.color.Color(wall_color)
         self._speed = speed # framerate in fps
         self._speed_inc = speed_inc # speed increment
+        self._save_update = save_update # avoid direction conflicts
+        self._direction = 0
         
         self._screen = pygame.display.get_surface()
         self._screen_size = self._screen.get_size()
@@ -42,21 +44,17 @@ class Snake:
         return self._direction
         
     @direction.setter
-    def direction(self, new_direction, advanced=False):
+    def direction(self, new_direction):
         """Set a new direction."""
-        if advanced:
+        if self._save_update:
             if new_direction == pygame.K_UP and self.direction != pygame.K_DOWN:
-                self._direction = new_direction
-                print("UP")
+                self._direction = new_direction                
             elif new_direction == pygame.K_LEFT and self.direction != pygame.K_RIGHT:
                 self._direction = new_direction
-                print("LEFT")        
             elif new_direction == pygame.K_RIGHT and self.direction != pygame.K_LEFT:
-                self._direction = new_direction
-                print("RIGHT")
+                self._direction = new_direction                
             elif new_direction == pygame.K_DOWN and self.direction != pygame.K_UP:
-                self._direction = new_direction        
-                print("DOWN")                
+                self._direction = new_direction                        
         else:
             self._direction = new_direction
 
@@ -64,7 +62,7 @@ class Snake:
         self.snake.clear()
         for ii in range (0,5):
             self.snake.append((int(self._screen_size[0]/4 + ii*self._sprite_size[0]), int(self._screen_size[1]/2)))
-        self._direction = pygame.K_RIGHT
+        self._direction = pygame.K_RIGHT # force update
 
     def do(self, event_key):
         if event_key in {pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT}:
---
+++ tests/test_snake.py
@@ -0,0 +1,240 @@
+# Yet Another Snake Game feat. Q-Learning by Michael Zenge
+#
+# Run unit tests in project directory with "python -m tests.test_snake"
+#
+
+import unittest
+import pygame
+
+from snake import *
+
+class test_snake(unittest.TestCase):        
+    def setUp(self):
+        self._sprite_size = (16,16)
+        self._screen_size = (20*self._sprite_size[0], 16*self._sprite_size[1])        
+        self._screen = pygame.display.set_mode(self._screen_size)                
+        return super().setUp()
+    
+    def tearDown(self):
+        return super().tearDown()
+    
+    def test_sprite_size(self):
+        """Test 'sprite_size' property"""
+        snake = Snake(self._sprite_size, 'darkgreen', 'green', 'red', 'dimgrey')
+        self.assertEqual(snake.sprite_size, self._sprite_size)
+
+    def test_body_color(self):
+        """Test selection of supported color formats"""        
+        # Integer value
+        color_int = 0xFFFFFFFF # (255, 255, 255, 255) -> white
+        white_snake = Snake(self._sprite_size, 'darkgreen', color_int, 'red', 'dimgrey')
+        self.assertIsInstance(white_snake.body_color, pygame.color.Color)
+        self.assertEqual(white_snake.body_color, pygame.color.Color(color_int))
+
+        # (r, g, b, [a])
+        color_rgb = (255, 0, 0) # red
+        red_snake = Snake(self._sprite_size, 'darkgreen', color_rgb, 'red', 'dimgrey')
+        self.assertIsInstance(red_snake.body_color, pygame.color.Color)
+        self.assertEqual(red_snake.body_color, pygame.color.Color(color_rgb))
+
+        # Named color
+        color_str = 'green'
+        green_snake = Snake(self._sprite_size, 'darkgreen', color_str, 'red', 'dimgrey')
+        self.assertIsInstance(green_snake.body_color, pygame.color.Color)
+        self.assertEqual(green_snake.body_color, pygame.color.Color(color_str))
+
+        # HTML color format
+        color_str = '#0000FF' # blue
+        blue_snake = Snake(self._sprite_size, 'darkgreen', color_str, 'red', 'dimgrey')
+        self.assertIsInstance(blue_snake.body_color, pygame.color.Color)
+        self.assertEqual(blue_snake.body_color, pygame.color.Color(color_str))
+
+    def test_wall_color(self):
+        """Test selection of supported color formats"""        
+        # Integer value
+        color_int = 0xFFFFFFFF # (255, 255, 255, 255) -> white
+        wall_color = Snake(self._sprite_size, 'darkgreen', 'green', 'red', color_int).wall_color
+        self.assertIsInstance(wall_color, pygame.color.Color)
+        self.assertEqual(wall_color, pygame.color.Color(color_int))
+
+        # (r, g, b, [a])
+        color_rgb = (255, 0, 0) # red
+        wall_color = Snake(self._sprite_size, 'darkgreen', 'green', 'red', color_rgb).wall_color
+        self.assertIsInstance(wall_color, pygame.color.Color)
+        self.assertEqual(wall_color, pygame.color.Color(color_rgb))
+
+        # Named color
+        color_str = 'green'
+        wall_color = Snake(self._sprite_size, 'darkgreen', 'green', 'red', color_str).wall_color
+        self.assertIsInstance(wall_color, pygame.color.Color)
+        self.assertEqual(wall_color, pygame.color.Color(color_str))
+
+        # HTML color format
+        color_str = '#0000FF' # blue
+        wall_color = Snake(self._sprite_size, 'darkgreen', 'green', 'red', color_str).wall_color
+        self.assertIsInstance(wall_color, pygame.color.Color)
+        self.assertEqual(wall_color, pygame.color.Color(color_str))
+
+    def test_direction_without_save_update(self):
+        """Test 'direction' property without save update"""        
+        snake = Snake(self._sprite_size, 'darkgreen', 'green', 'red', 'dimgrey', save_update = False)
+        
+        # Snake shall move in right direction after initialization
+        self.assertEqual(snake.direction, pygame.K_RIGHT)
+
+        # Try up and down
+        snake.direction = pygame.K_UP
+        self.assertEqual(snake.direction, pygame.K_UP)
+        snake.direction = pygame.K_DOWN
+        self.assertEqual(snake.direction, pygame.K_DOWN)
+
+         # Try left and right
+        snake.direction = pygame.K_LEFT
+        self.assertEqual(snake.direction, pygame.K_LEFT)
+        snake.direction = pygame.K_RIGHT
+        self.assertEqual(snake.direction, pygame.K_RIGHT)
+
+        #TODO: Test other key events ...
+
+    def test_direction_with_save_update(self):
+        """Test 'direction' property with save update"""        
+        snake = Snake(self._sprite_size, 'darkgreen', 'green', 'red', 'dimgrey', save_update = True)
+        
+        # Snake shall move in right direction after initialization
+        self.assertEqual(snake.direction, pygame.K_RIGHT)
+
+        # Try up and down
+        snake.direction = pygame.K_UP
+        self.assertEqual(snake.direction, pygame.K_UP)
+        snake.direction = pygame.K_DOWN
+        self.assertEqual(snake.direction, pygame.K_UP)
+
+         # Try left and right
+        snake.direction = pygame.K_LEFT
+        self.assertEqual(snake.direction, pygame.K_LEFT)
+        snake.direction = pygame.K_RIGHT
+        self.assertEqual(snake.direction, pygame.K_LEFT)
+
+        # Try down and up
+        snake.direction = pygame.K_DOWN
+        self.assertEqual(snake.direction, pygame.K_DOWN)
+        snake.direction = pygame.K_UP
+        self.assertEqual(snake.direction, pygame.K_DOWN)
+
+         # Try right and left
+        snake.direction = pygame.K_RIGHT
+        self.assertEqual(snake.direction, pygame.K_RIGHT)
+        snake.direction = pygame.K_LEFT
+        self.assertEqual(snake.direction, pygame.K_RIGHT)
+
+        #TODO: Test other key events ...
+
+    def test_reset(self):
+        """Test 'reset' of snake"""
+        snake = Snake(self._sprite_size, 'darkgreen', 'green', 'red', 'dimgrey', 10, 0.5, save_update = True)        
+        # Change direction
+        snake.direction = pygame.K_UP
+        snake.direction = pygame.K_LEFT        
+        # Reset
+        snake.reset()
+        # Test
+        for ii in range (0,5):
+            self.assertEqual(snake.snake[ii], (int(self._screen_size[0]/4 + ii*self._sprite_size[0]), int(self._screen_size[1]/2)))
+        self.assertEqual(snake.direction, pygame.K_RIGHT)
+
+    def test_do(self):
+        """Test 'do' method"""
+        snake = Snake(self._sprite_size, 'darkgreen', 'green', 'red', 'dimgrey')
+        for key_event in {pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT}:
+            snake.do(key_event)
+            self.assertEqual(snake.direction, key_event)
+        for key_event in {pygame.K_SPACE, pygame.K_ESCAPE}:
+            snake.do(key_event)
+            self.assertNotEqual(snake.direction, key_event)
+
+    def test_update(self):
+        """Test 'update'method"""
+        bkgrd_color = pygame.color.Color('black') 
+        head_color = pygame.color.Color('darkgreen')
+        body_color = pygame.color.Color('green')
+        apple_color = pygame.color.Color('red')
+        wall_color = pygame.color.Color('dimgrey')
+
+        for color in [bkgrd_color, head_color, body_color, apple_color, wall_color]:
+            snake = Snake(self._sprite_size, head_color, body_color, apple_color, wall_color)
+            snake_len = len(snake.snake[:])
+        
+            # Reset background color
+            self._screen.fill(color)
+            result = snake.update()
+
+            # Collision
+            if self._screen.get_at(snake.snake[-1]) in [body_color, wall_color]:
+                self.assertFalse(result)
+            else:
+                self.assertTrue(result)
+
+            # Grow snake
+            if self._screen.get_at(snake.snake[-1]) == apple_color:
+                self.assertEqual(len(snake.snake[:]), snake_len+1)
+            else:
+                self.assertEqual(len(snake.snake[:]), snake_len)
+
+    def test_draw(self):
+        """Test 'draw' method"""
+        bkgrd_color = pygame.color.Color('black') 
+        head_color = pygame.color.Color('darkgreen')
+        body_color = pygame.color.Color('green')
+        apple_color = pygame.color.Color('red')
+        wall_color = pygame.color.Color('dimgrey')
+
+        for color in [bkgrd_color, head_color, body_color, apple_color, wall_color]:
+            snake = Snake(self._sprite_size, head_color, body_color, apple_color, wall_color)
+
+            # Reset background color
+            self._screen.fill(bkgrd_color)
+            snake.draw()
+
+            # Test color of body elements at position
+            for pos in snake.snake[0:-1]:
+                self.assertEqual(self._screen.get_at(pos), body_color)
+            
+            # Test color at head position
+            match color:
+                case bkgrd_color, head_color:
+                    self.assertEqual(self._screen.get_at(snake.snake[-1]), head_color)
+                case body_color, apple_color, wall_color:
+                    self.assertEqual(self._screen.get_at(snake.snake[-1]), color)
+    
+    def test_move(self):
+        """Test 'move' method"""
+        snake = Snake(self._sprite_size, 'darkgreen', 'green', 'red', 'dimgrey')
+        
+        for direction in {pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT}:            
+            new_snake = Snake(self._sprite_size, 'darkgreen', 'green', 'red', 'dimgrey')
+            new_snake.direction = direction
+            
+            new_snake.move()       
+            
+            # Compare body elements     
+            for ii in range (0,4):
+                self.assertEqual(new_snake.snake[ii], snake.snake[ii+1])
+            
+            # Compare head position
+            if direction == pygame.K_RIGHT:
+                 self.assertEqual(new_snake.snake[-1][0], snake.snake[-1][0] + self._sprite_size[0])
+                 self.assertEqual(new_snake.snake[-1][1], snake.snake[-1][1])
+            elif direction == pygame.K_UP:
+                self.assertEqual(new_snake.snake[-1][0], snake.snake[-1][0])
+                self.assertEqual(new_snake.snake[-1][1], snake.snake[-1][1] - self._sprite_size[1])
+            elif direction == pygame.K_DOWN:
+                self.assertEqual(new_snake.snake[-1][0], snake.snake[-1][0])
+                self.assertEqual(new_snake.snake[-1][1], snake.snake[-1][1] + self._sprite_size[1])
+            elif direction == pygame.K_LEFT:        
+                self.assertEqual(new_snake.snake[-1][0], snake.snake[-1][0] - self._sprite_size[0])
+                self.assertEqual(new_snake.snake[-1][1], snake.snake[-1][1])
+        
+if __name__ == "__main__":
+    unittest.main()
+
+# Copyright (c) 2024 michael-zenge, permission granted under MIT license
