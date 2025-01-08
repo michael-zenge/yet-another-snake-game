1c4de119c0172065244b153628f398fa60f8ff8f
michael-zenge <michael.zenge@gmail.com>
2025-01-08 20:28:18 +0100

    Docstrings added

--- tests/test_q_learning.py
+++ tests/test_q_learning.py
@@ -27,7 +27,8 @@ class test_q_learning(unittest.TestCase):
     def tearDown(self):
         return super().tearDown()
     
-    def test_update(self):        
+    def test_update(self):
+        """Test change of direction after update"""        
         env = SnakeQLearning(self._apple, self._snake, 0.0, 0.2, 0.5, 1000)        
         orig_direction = env._snake.direction
         self.assertEqual(orig_direction, pygame.K_RIGHT)
@@ -36,15 +37,18 @@ class test_q_learning(unittest.TestCase):
         self.assertEqual(new_direction, pygame.K_LEFT)
 
     def test_reward(self):
+        """Test reward method"""
         env = SnakeQLearning(self._apple, self._snake, 0.0, 0.2, 0.5, 1000)
         self.assertEqual(env.reward(), 1)
         
     def test_get_max_reward(self):
+        """Test calcuation of maximum reward"""
         env = SnakeQLearning(self._apple, self._snake, 0.0, 0.2, 0.5, 1000)
         self.assertAlmostEqual(env._get_max_reward(0), 0.95, 2)
         self.assertAlmostEqual(env._get_max_reward(4095), 0.98, 2)
     
     def test_get_reward(self):
+        """Test reward for selected states"""
         env = SnakeQLearning(self._apple, self._snake)
         self.assertEqual(env._get_reward(4095,0), -10)
         self.assertEqual(env._get_reward(3071,0), -10)
@@ -62,6 +66,7 @@ class test_q_learning(unittest.TestCase):
         self.assertEqual(env._get_reward(0,0), -1)
     
     def test_get_reward_with_location_offset(self):
+        """Test reward for selected state-action pairs"""
         env = SnakeQLearning(self._apple, self._snake)
         for action_idx in range(0,4):
             self.assertEqual(env._get_reward_with_location_offset(512, action_idx, pygame.K_DOWN, pygame.K_RIGHT, 0), -10)
@@ -70,6 +75,7 @@ class test_q_learning(unittest.TestCase):
         self.assertEqual(env._get_reward_with_location_offset(0, 3, pygame.K_DOWN, pygame.K_RIGHT, 0), -1)
 
     def test_action(self):
+        """Test return of '_action' method"""
         # exploration_rate = 0.0
         env = SnakeQLearning(self._apple, self._snake, 0.0, 0.2, 0.5, 1000)
         self.assertEqual(env._action(), pygame.K_DOWN)
@@ -78,6 +84,7 @@ class test_q_learning(unittest.TestCase):
         self.assertEqual(env._action(), pygame.K_RIGHT)
     
     def test_call_agent_for_action(self):
+        """Test actions for selected states"""
         env = SnakeQLearning(self._apple, self._snake, 0.2, 0.2, 0.5, 1000)
         self.assertEqual(env._call_agent_for_action(0), 2)
         self.assertEqual(env._call_agent_for_action(512), 3)
@@ -85,8 +92,8 @@ class test_q_learning(unittest.TestCase):
         self.assertEqual(env._call_agent_for_action(4095), 1)
     
     def test_get_state_idx(self):
+        """Test state depending on relative positions of snake and apple only"""
         env = SnakeQLearning(self._apple, self._snake)
-
         self._apple.reset(3000)
         self.assertEqual(env._get_state_idx(), 0)
         self._apple.reset(2000)
@@ -97,14 +104,13 @@ class test_q_learning(unittest.TestCase):
         self.assertEqual(env._get_state_idx(), 3072)
     
     def test_get_state_idx_with_offset(self):
+        """Test state depending on the color at current location"""
         env = SnakeQLearning(self._apple, self._snake)
-
         for color in Color:
             self._screen.fill(color.value)
             self._snake.draw()
             match color:
                 case Color.WALL:
-
                     self.assertEqual(env._get_state_idx_with_offset(0, 0, 0), 3)
                     self.assertEqual(env._get_state_idx_with_offset(0, 0, 2), 12)
                     # Snake body to the left
